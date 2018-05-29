35efdb4a2ced6153db160e666cf64e635b83cee4
Suzanne Vogt <svogt@sumarc.net>
2018-05-29 16:47:37 -0400

    Make sure js controlling +/- in RR is loaded after the RR is loaded.

--- js/unh_d1_drupal.sections_pane.js
+++ js/unh_d1_drupal.sections_pane.js
@@ -2,7 +2,6 @@
   Drupal.behaviors.unh_d1_drupal = {
     attach: function (context, settings) {
       unh_d1_drupal_ajax_load(settings.nid, settings.programArea, settings.courseCode, settings.conf, settings.base_url);
-      // console.log('SETTINGS: ' + JSON.stringify(settings));
     }
   };
 })(jQuery);
@@ -15,10 +14,16 @@ function unh_d1_drupal_ajax_load(nid, programArea, courseCode, conf, base_url) {
     // console.log('base_url: ' + base_url)
     jQuery(".unh-d1-drupal--d1--course-sections-pane").hide();
     random_num = Math.floor(Math.random() * 1000) + 1;
-    jQuery("#ajax-target").load(base_url + "/unh_d1_drupal/ajax/get_section_status/" + nid + '/' + programArea + '/' + courseCode + '/' + random_num, conf);
+    jQuery("#ajax-target").load(base_url + "/unh_d1_drupal/ajax/get_section_status/" + nid + '/' + programArea + '/' + courseCode + '/' + random_num, conf, 
+      function() {
+        // This gets executed when the content is loaded
+        jQuery( ".course-page .card .card-header a" ).click(function() {
+          jQuery(this).find('.fa').toggle();
+        });
+      }
+    );
   } else {
       // console.log('NOT NOT NOT - MAKING THE Call!!'); 
-
       jQuery(".unh-d1-drupal--d1--course-sections-pane").show();
   }
 }
